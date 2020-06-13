4800a35eefaf4a9df7ec72ed5f4771bd929f3f57
Riels <289770334@qq.com>
2020-06-13 17:06:13 +0800

    gen ref path

--- visualize_ref_path.py
+++ visualize_ref_path.py
@@ -4,9 +4,11 @@ import matplotlib.patches as patches
 import scipy.io as scio
 import numpy as np
 import math
+from scipy.optimize import leastsq
+from align_ref_img import counterclockwise_rotate
 
 
-def plot_ref_path(map_file, all_points, circle_point):
+def plot_raw_ref_path(map_file, all_points, circle_point):
     fig, axes = plt.subplots(1, 1)
     map_vis_without_lanelet.draw_map_without_lanelet(map_file, axes, 0, 0)
     for way_points in all_points[0, :]:
@@ -21,6 +23,105 @@ def plot_ref_path(map_file, all_points, circle_point):
     plt.show()
 
 
+def plot_ref_path(map_file, ref_path_points):
+    fig, axes = plt.subplots(2, 3)
+    start = dict()
+    i = -1
+    keys = sorted(ref_path_points.keys())
+    for k in keys:
+        v = ref_path_points[k]
+        st = k.split('-')[0]
+        if st not in start:
+            i += 1
+            start[st] = 1
+            map_vis_without_lanelet.draw_map_without_lanelet(map_file, axes[i//3][i % 3], 0, 0)
+        xp = [p[0] for p in v]
+        yp = [p[1] for p in v]
+        axes[i // 3][i % 3].plot(xp, yp, linewidth=4)
+    plt.show()
+
+
+def residuals(p, x, y):
+    a, b, r = p
+    return r**2 - (y - b) ** 2 - (x - a) ** 2
+
+
+def fit_circle(circle_point):
+    circle_x = []
+    circle_y = []
+    for p in circle_point:
+        if math.isnan(p[0][0]):
+            continue
+        circle_x.append(p[0][0])
+        circle_y.append(p[0][1])
+    x = np.array(circle_x)
+    y = np.array(circle_y)
+    result = leastsq(residuals, np.array([1, 1, 1]), args=(x, y))
+    a, b, r = result[0]
+    r = max(-r, r)
+    print("a=", a, "b=", b, "r=", r)
+    cx = [a-r]
+    cy = [b]
+    for i in range(359):
+        nx, ny = counterclockwise_rotate(a-r, b, (a, b), (i+1)*math.pi/180)
+        cx.append(nx)
+        cy.append(ny)
+    return cx, cy
+
+
+def nearest_c_point(p, cx, cy):
+    min_dis = 1e8
+    xn, yn = 0, 0
+    min_i = 0
+    for i in range(len(cx)):
+        x, y = cx[i], cy[i]
+        if (x-p[0])**2 + (y-p[1])**2 < min_dis:
+            min_dis = (x-p[0])**2 + (y-p[1])**2
+            xn, yn = x, y
+            min_i = i
+    return min_i, xn, yn
+
+
+def get_ref_path(data):
+    ref_path_points = dict()
+    para_path = data['para_path'][0]
+    circle_merge_point = data['circle_merge_point'][0]
+    branchID = data['branchID'][0]
+    pre = dict()
+    post = dict()
+
+    for i in range(len(branchID)):
+        s = branchID[i][0]
+        if s[-1] == -1:
+            min_i, _, _ = nearest_c_point(circle_merge_point[i][0], cx, cy)
+            d = {'min_i': min_i, 'path': para_path[i]}
+            pre[str(s[0])] = d
+        elif s[0] == -1:
+            min_i, _, _ = nearest_c_point(circle_merge_point[i][0], cx, cy)
+            d = {'min_i': min_i, 'path': para_path[i]}
+            post[str(s[1])] = d
+        else:
+            label = str(s[0]) + '-' + str(s[1])
+            ref_path_points[label] = para_path[i]
+
+    for k1, v1 in pre.items():
+        for k2, v2 in post.items():
+            if k1+'-'+k2 in ref_path_points.keys():
+                continue
+            label = k1+'--1-'+k2
+            i1 = v1['min_i']
+            i2 = v2['min_i']
+            if i2 > i1:
+                cpx = cx[i1:i2+1]
+                cpy = cy[i1:i2+1]
+            else:
+                cpx = cx[i1:] + cx[:i2+1]
+                cpy = cy[i1:] + cy[:i2+1]
+            cp = np.array([[x, y] for x, y in zip(cpx, cpy)])
+            ref_path_points[label] = np.vstack((v1['path'], cp, v2['path']))
+    return ref_path_points
+
+
 if __name__ == '__main__':
     map_dir = 'D:/Downloads/INTERACTION-Dataset-DR-v1_0/maps/'
     map_name = "DR_USA_Roundabout_FT.osm"
@@ -28,4 +129,8 @@ if __name__ == '__main__':
     data = scio.loadmat(dataFile)
     para_path = data['Segmented_reference_path']['para_path']
     circle_merge_point = data['Segmented_reference_path']['circle_merge_point'][0]
-    plot_ref_path(map_dir+map_name, para_path, circle_merge_point)
+    plot_raw_ref_path(map_dir + map_name, para_path, circle_merge_point)
+
+    cx, cy = fit_circle(circle_merge_point)
+    ref_path_points = get_ref_path(data['Segmented_reference_path'])
+    plot_ref_path(map_dir + map_name, ref_path_points)
