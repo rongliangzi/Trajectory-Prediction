aed11de9edd4ab93447898a6b2b5b342cbbe0c90
Riels <289770334@qq.com>
2020-07-17 13:42:23 +0800

    finish EPT

--- gen_EP_T.py
+++ gen_EP_T.py
@@ -1,4 +1,4 @@
 from utils.EP_utils import *
 if __name__ == '__main__':
-    func_file = 'D:/Dev/UCB task/DR_USA_Roundabout_EP/ref_path_funcs.txt'
+    func_file = 'D:/Dev/UCB task/USA_Roundabout_EP_final/ref_path_funcs.txt'
     read_funcs(func_file)
